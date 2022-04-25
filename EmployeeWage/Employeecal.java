a7582c4d2251e88265122adac9b3ae77db69b350
amartarocks3 <amartarocks3@gmail.com>
2022-04-25 23:28:48 +0530

    Added: Employee wage till 20 day or 100 hrs

--- EmployeeWage/Employeecal.java
+++ EmployeeWage/Employeecal.java
@@ -5,13 +5,17 @@ public class Employeecal {
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
 	
 	public static void main(String[] args) {
 		int empHrs = 0;
 		int empWage = 0;
 		int totalEmpWage = 0;
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++) {
+		int totalDays = 0;
+		int totalHrs=0;
+		while(totalDays<=NUM_OF_WORKING_DAYS && totalHrs<=MAX_HRS_IN_MONTH) {
 		int empCheck = (int) Math.floor(Math.random()*10)%3;
+		totalDays++;
 		switch(empCheck) {
 		case IS_PART_TIME:
 			empHrs = 4;
@@ -22,6 +26,7 @@ public class Employeecal {
 		default:
 			empHrs = 0;
 		}
+		totalHrs+=empHrs;
 		empWage = empHrs*EMP_RATE_PER_HOUR;
 		totalEmpWage=totalEmpWage+empWage;
 		System.out.println(empWage);
