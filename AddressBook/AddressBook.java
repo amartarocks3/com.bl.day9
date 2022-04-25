f340ddd11c9ba335a93df642bdcec021b6cde41d
amartarocks3 <amartarocks3@gmail.com>
2022-04-26 00:56:25 +0530

    added: Address book

---
+++ AddressBook/AddressBook.java
@@ -0,0 +1,26 @@
+package AddressBook;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class AddressBook implements AddressBookIF{
+	
+	ArrayList <ContactPerson> contactList = new ArrayList<ContactPerson>();
+
+	@Override
+	public void addContact(ContactPerson person) {
+		contactList.add(person);
+		
+	}
+	
+	@Override
+	public void displayContents() {
+		
+		Iterator<ContactPerson> iterator = contactList.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+	}
+}
---
+++ AddressBook/AddressBookIF.java
@@ -0,0 +1,6 @@
+package AddressBook;
+
+public interface AddressBookIF {
+	public void addContact(ContactPerson person);
+	public void displayContents();
+}
